b572e2eada00fb8195980cc9aa8df5959b867b6c
Alex Koo <alexkoo213@gmail.com>
2017-07-28 20:53:27 +0800

    Another Try

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Because matrix inversion is usually a costly computation, there is benefitial
+# to caching the inverse of a matrix rather than compute it repeatedly to save the memory.
+# The following two functions are used to cache the inverse of a matrix.
 
-## Write a short comment describing this function
-
+## The first function makeCacheMatrix creates a list 
+# as a special vector containing a function to:
+# 1. set the value of the matrix
+# 2. get the value of the matrix
+# 3. set the value of inverse of the matrix
+# 4. get the value of inverse of the matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+        inverse <- NULL
+        setmatrix <- function(y){
+                x <<- y
+                inverse <<- NULL
+        }
+        getmatrix <- function() x
+        setinv <- function(inv) inverse <<- inv
+        getinv <- function() inverse
+        list(setmatrix = setmatrix, getmatrix = getmatrix,
+             setinv = setinv,
+             getinv = getinv)
 }
 
+## The cacheSolve function calculates the inverse of the 
+# matrix created with the makeCacheMatrix function. 
+# It first checks to see if the inverse has already been calculated.
+# If so, it gets it directly from the cache and skips the computation. 
+# If not, it calculates the inverse of the matrix 
+# and sets the value of the inverse in the cache via the setinv function.
 
-## Write a short comment describing this function
+## Caution: This function assumes that the matrix supplied is always invertible.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inverse <- x$getinv()
+        if(!is.null(inverse)) {
+                message("getting cached data")
+                return(inverse)
+        }
+        data <- x$getmatrix()
+        inverse <- slove(data, ...)
+        x$setinv(inverse)
+        inverse
 }
